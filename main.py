d0781fc054baaeed88f8eed605cb879381c8e065
Michael Davis <745953+mbd0910@users.noreply.github.com>
2024-10-24 19:39:31 +0100

    Split logic into functions, remove all new lines

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
 import httpx
 from selectolax.parser import HTMLParser
 
-url = "https://www.emp.co.uk/band-merch/t-shirts/"
+def get_html():
+    url = "https://www.emp.co.uk/band-merch/t-shirts/"
 
-headers = {
-    "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/129.0.0.0 Safari/537.36"
-}
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/129.0.0.0 Safari/537.36"
+    }
 
-response = httpx.get(url, headers=headers)
-html = HTMLParser(response.text)
+    response = httpx.get(url, headers=headers)
+    html = HTMLParser(response.text)
+    return html
 
 def extract_text(html, selector):
     try:
@@ -16,23 +18,38 @@ def extract_text(html, selector):
     except AttributeError:
         return None
 
-products = html.css('div.grid-tile')
+def remove_new_lines(str: str):
+    return str.replace('\n', '')
 
-for product in products:
-    product_name_spans = product.css('div.product-name span')
-    parts = []
-    for span in product_name_spans:
-        if span.attributes['class'] == 'spacer':
-            parts.append(' / ')
-        else:
-            parts.append(span.text().removeprefix('\n').removesuffix('\n'))
-    product_name = str.join(' ', parts)
-    product_price = extract_text(product, 'span.currentprice')
+def parse_page(html):
+    products = html.css('div.grid-tile')
+    items = []
 
-    item = {
-        'name': product_name,
-        'price': product_price
-    }
+    for product in products:
+        product_name_spans = product.css('div.product-name span')
+        parts = []
+        for span in product_name_spans:
+            if span.attributes['class'] == 'spacer':
+                parts.append(' / ')
+            else:
+                parts.append(remove_new_lines(span.text()))
+        product_name = str.join(' ', parts)
+        product_price = extract_text(product, 'span.currentprice')
+        if product_price is not None:
+            product_price = remove_new_lines(product_price)
 
-    print(item)
+        item = {
+            'name': product_name,
+            'price': product_price
+        }
 
+        items.append(item)
+
+    return items
+
+def main():
+    items = parse_page(get_html())
+    print(items)
+
+if __name__ == '__main__':
+    main()
